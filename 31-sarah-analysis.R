b1082e3e31b4912fd79e09c2720ed715f1a91aa8
Eduard Bukin <edwardbukin@gmail.com>
2021-07-12 18:56:18 +0200

    Slowly modifyong Sarah' analysis

--- 31-sarah-analysis.R
+++ 31-sarah-analysis.R
@@ -1,61 +1,199 @@
-
-
-# Setup
-## install ("pacman") # This package will install amd prepare packages in the lines below:
-
-
+# Eduard's post processing of Sarah's code
 library(pacman)
-library (dplyr)
-pacman::p_load(sf, tidyverse, rgdal, mapview)
-# I use sf and tidyverse for data manipulation.
+pacman::p_load(sf, tidyverse, rgdal, rmapshaper, mapview, here, tidytext)
 
-rm(list=ls()) 
-
-setwd ("C:\\Users\\sarah\\Documents\\IAMO\\Fieldwork\\SPATIAL_DATA\\Kazakhstan\\Cadastre_GIS\\Cadastre_rds\\")
 
 # Loading data -----------------------------------------------------------------
 
-# Use rds instead of shapefiles
-# We immediately convert the csr:
-# This file 
-
-plots_geoms <- read_rds("plot-geom-api-meta.rds") %>% 
+plots_geoms <- 
+  read_rds("data-clean/10.1-sarah-request-clean/plot-geom-api-meta.rds") %>% 
   st_transform(st_crs(3857))
 
-# No need to transform because it is transformed:
-plots_ugods_geoms <- read_rds("plot-geom-ugod-areas.rds") 
-st_crs(plots_ugods_geoms) == st_crs(plots_geoms)
+plots_ugods_geoms <- read_rds("data-clean/10.1-sarah-request-clean/plot-geom-ugod-areas.rds") 
+
+rayon_bounds <- 
+  read_rds("data-clean/02-rayon-shapes/kaz-rayons-shapes_2021-03-31-13-24-01.rds") %>% 
+  filter(name %in% c("Kegenskijrajon", "Enbeksikazahskijrajon", "Rajymbekskijrajon")) %>% 
+  select(rajon = raion, obl_kaz, raj_rus )
+
+rayon_bounds_union <- rayon_bounds %>% st_make_valid() %>% st_union() %>% st_make_valid()
 
 
-# Analysis -----------------------------------------------------------------
 
-# Showing data without geometries:
-plots_geoms %>% 
-  st_drop_geometry() %>% 
-  as_tibble() %>% 
-  glimpse
-
-
-# Plotting data with colours by rayons. 
-# May take some time:
-plots_geoms %>% 
-  filter(rayon_rus %in% c("Kegenskij")) %>% 
+# # Loading and clipping open street map data
+# # Because this is a time-intensive process, we will save intermediate results.
+# buildings_full <-
+#   sf::st_read("data-clean/08-osm/gis_osm_buildings_a_free_1.shp") %>% 
+#   select(osm_id, code, fclass) %>% 
+#   st_transform(st_crs(3857))
+# bl_cntrd <-
+#   buildings_full %>%
+#   mutate(centroid = st_centroid(st_geometry(.))) %>%
+#   st_set_geometry("centroid") %>%
+#   select(-geometry) %>%
+#   mutate(in_buffer = as.logical(st_intersects(
+#     ., st_buffer(rayon_bounds_union, 1) , sparse = F
+#   ))) %>%
+#   filter(in_buffer) %>%
+#   st_drop_geometry() %>%
+#   as_tibble()
+# buildings_obl <-
+#   buildings_full %>% 
+#   filter(osm_id %in% bl_cntrd$osm_id) %>% 
+#   rmapshaper::ms_clip(st_as_sf(rayon_bounds_union))
+# buildings_obl %>% 
+#   write_rds(here::here('data-temp', 'buildings_3_rajons.rds'), compress = "gz")
+buildings_obl <- read_rds(here::here('data-temp', 'buildings_3_rajons.rds'))
+buildings_obl %>%
   ggplot() + 
-  aes(fill = rayon_rus) + 
-  geom_sf()
+  geom_sf(aes(fill = fclass), colour = NA) + 
+  geom_sf(data = rayon_bounds, fill = NA)
+
+
+# # Lanfuse data first - not too informative
+# landuse_full <-
+#   sf::st_read("data-clean/08-osm/gis_osm_landuse_a_free_1.shp") %>% 
+#   select(osm_id, code, fclass) %>% 
+#   st_transform(st_crs(3857))
+# landuse_obl <-
+#   landuse_full %>% 
+#   rmapshaper::ms_clip(bbox = st_bbox(rayon_bounds_union)) %>% 
+#   rmapshaper::ms_clip(st_as_sf(rayon_bounds_union))
+# landuse_obl %>% 
+#   write_rds(here::here('data-temp', 'landuse_3_rajons.rds'), compress = "gz")
+landuse_obl <- 
+  read_rds(here::here('data-temp', 'landuse_3_rajons.rds'))
+
+# important of different land classes:
+landuse_obl %>% 
+  mutate(area = as.numeric(st_area(.)) / 1000 / 1000) %>% 
+  st_drop_geometry() %>% 
+  group_by(fclass) %>% 
+  summarise(area = sum(area, na.rm = T)) %>% 
+  ungroup() %>% 
+  arrange(desc(area))
+  
+
+landuse_obl %>% 
+  filter(fclass %in% "residential") %>% 
+  ggplot() + 
+  geom_sf(aes(fill = fclass), colour = NA) + 
+  geom_sf(data = rayon_bounds, fill = NA)
+
+# # Roads - too heavy to have it.
+# roads <- sf::st_read("data-clean/08-osm/gis_osm_roads_free_1.shp")
+# roads_obl <- 
+#   roads %>% 
+#   rmapshaper::ms_clip(bbox = st_bbox(rayon_bounds_union))
+
+# # Former irrelevant checks
+# st_crs(plots_ugods_geoms) == st_crs(plots_geoms)
+# st_crs(plots_ugods_geoms) == st_crs(rayon_bounds)
+# st_crs(plots_ugods_geoms) == st_crs(buildings_full)
+# st_crs(plots_ugods_geoms) == st_crs(landuse_full)
 
 
 
 # Getting summary about ugodias types and plots---------------------------
 
-plots_ugods_geoms %>% 
-  select(cadastre_id, plot_area, contains("ugod"))
+# plots_ugods_geoms %>% 
+#   select(cadastre_id, plot_area, contains("ugod"))
+# 
+# # Interactive map
+# # Takes time too
+# plots_ugods_geoms %>% 
+#   mapview(zcol = "rayon_rus")
 
 
-# Interactive map
-# Takes time too
-plots_ugods_geoms %>% 
-  mapview(zcol = "rayon_rus")
+# Understanding what is behin  meta_celevoe_naznacenie ------------------------
+# Exploring variants of the "meta_celevoe_naznacenie" field
+ugod_mtdt_df <- 
+  plots_ugods_geoms %>% 
+  st_drop_geometry() %>% 
+  as_tibble() %>% 
+  select(new_meta = meta_celevoe_naznacenie) 
+
+# Checking words frequency
+ugod_mtdt_df %>% 
+  unnest_tokens(word, new_meta) %>%
+  count(word, sort = TRUE)
+
+
+# Actual summarizing. Will be joint to the data late. We rename variable for shorter names.
+# All cadastre_id are unique!
+plot_naznachenie <- 
+  plots_ugods_geoms %>% 
+  st_drop_geometry() %>% 
+  as_tibble() %>% 
+  select(cadastre_id, new_meta = meta_celevoe_naznacenie) %>% 
+  mutate(
+    new_meta = str_to_lower(new_meta),
+    new_meta =
+      case_when(
+        str_detect(new_meta, "podsobno|obnogo|podsosbnogo|podsobonogo|podsobngo|podsounogo") |
+          (str_detect(new_meta, "lic") & str_detect(new_meta, "hozaj")) ~ 
+          "ag_hh_podsob",
+        
+        str_detect(new_meta, "krest|fermers|kroest|kreas|kores|korest") ~ 
+          "ag_IF_krest",
+        str_detect(new_meta, "sadovod|sadav|sadoa|sadov|saadov|sada|sadlov|sadorv") ~ 
+          "ag_gardens",
+        str_detect(new_meta, "tovarnogo") ~ "ag_prod",
+        str_detect(new_meta, "sel.sko") & !str_detect(new_meta, "obslu|ustanov|klub|maga|obsz") ~ "ag_other",
+        
+        str_detect(new_meta, "stroitel") ~ "noag_const",
+        str_detect(new_meta, "bsluz|magazi|ustanov|klub|razmes|kompl|obsz|oblsluz") ~ "noag_service",
+        str_detect(new_meta, "ohotnic") ~ "other_other", #"other_hunting",
+        str_detect(new_meta, "lesnogo|leso") ~  "other_forest", #"other_forest",
+        str_detect(new_meta, "forele|rybno|prudov") ~  "other_fish", #"other_fish",
+        TRUE ~ "other_other"
+      )
+  ) %>% 
+  rename(cel_nazn = new_meta)
+
+
+# Summarizing "aggregated celevoe naznachenie"
+plot_naznachenie %>%
+  count(cel_nazn_groupped, sort = TRUE) 
+
+
+
+# Understanding what is behind "meta_kategoria_zemel" ------------------------
+plot_land_category <- 
+  plots_ugods_geoms %>% 
+  st_drop_geometry() %>% 
+  as_tibble() %>% 
+  select(cadastre_id, kat = meta_kategoria_zemel) %>% 
+  mutate(
+    kat = case_when(
+      str_detect(kat, "naselennyh punktov") ~ "sett_land",
+      str_detect(kat, "skohozajstvenn") ~ "ag_land",
+      str_detect(kat, "promyslennosti") ~ "indust_land",
+      str_detect(kat, "zapasa") ~ "reserve_land",
+      str_detect(kat, "lesnogo") ~ "forest_land",
+      TRUE ~ "other"
+    )
+  ) %>% 
+  rename(land_kat = kat)
+
+plot_land_category %>% count(land_kat, sort = TRUE)
+  
+
+# Plotting the last one without settelments
+# TODO: Add the unallocated land
+land_cat_plot <-
+  plots_ugods_geoms %>% 
+  select(cadastre_id) %>% 
+  left_join(plot_land_category, by = "cadastre_id") %>% 
+  filter(land_kat != "sett_land")
+
+land_cat_plot %>% 
+  ggplot() + 
+  aes(fill = land_kat) + 
+  geom_sf(colour = NA) + 
+
+
+
 
 # Create categories for land tenure and ownership polygons
 
